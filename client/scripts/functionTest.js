6e708bc9b8431f5e4574c5da96c95c38de2eab80
stephandacosta <stephan.dacosta.tech@gmail.com>
2014-03-18 16:20:40 -0700

    refactored request handler, all LiveServerIntegration Specs pass, added light-weight client for testing (index+js)

---
+++ client/scripts/functionTest.js
@@ -0,0 +1,55 @@
+
+
+
+function postChat(message){
+$.ajax({
+  url: 'http://localhost:3000/1/classes/chatterbox',
+  type: 'POST',
+  data: message,
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    console.error('chatterbox: Failed to send message');
+  }
+});
+}
+
+
+function getChat(){
+  $.ajax({
+    url: 'http://localhost:3000/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      // parsedData = JSON.parse(data);
+      parsedData = data;
+      for (var i = 0; i < parsedData.results.length; i++) {
+        $('body').append($('<div>'));
+        $('body').append($('<div>').text(data));
+        $('body').append($('<div>').text("Username :" + parsedData.results[i].username));
+        $('body').append($('<div>').text("Text :" + parsedData.results[i].text));
+        $('body').append($('<div>').text("roomname :" + parsedData.results[i].roomname));
+      }
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+}
+
+
+var testObject = {
+    createdAt : "2013-10-07T16:22:03.280Z",
+    objectId : "teDOY3Rnpe",
+    roomname : "lobby",
+    text : "hello",
+    updatedAt : "2013-10-07T16:22:03.280Z",
+    username : "gary"
+};
+
+postChat(testObject);
+
+getChat();
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,47 +1,34 @@
-/* You should implement your request handler function in this file.
- * And hey! This is already getting passed to http.createServer()
- * in basic-server.js. But it won't work as is.
- * You'll have to figure out a way to export this function from
- * this file and include it in basic-server.js so that it actually works.
- * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-  var objContainer = [];
+var urlMessage = require("url");
+var http = require("http");
+var qs = require('querystring');
+var objContainer = [];
+
+var sendResponse = function(response, status, responseObj) {
+  var headers = {
+    "Content-Type" : "application/json",
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  };
+  response.writeHead(status, headers);
+  response.end(JSON.stringify(responseObj));
+};
+
 
 exports.handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
 
-  console.log("Serving request type " + request.method + "with request header " + request.headers['access-control-request-method'] + " for url " + request.url);
+  console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+  if (request.url !== "/classes/messages"){
+    sendResponse(response, 404, {});
+  }
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "text/plain";
-
-
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-  
-  var urlMessage = require("url");
-  var http = require("http");
-  var qs = require('querystring');
-
-
-  var method = request.headers['access-control-request-method']; //gives us the GET and POST
-  // console.log('request header :' , method);
   if (request.method === 'OPTIONS') {
-    // console.log('options method');
-  } 
+    sendResponse(response, 200, {});
+  }
 
   if (request.method === 'GET') {
-    // console.log('get method');
-    // We need to add in options for sending back objects based on requested URL
-    // console.log("THIS IS WHAT WE WANT:" + request.url + " END ");
-    //
     // var testObject = {results : [{
     //   createdAt : "2013-10-07T16:22:03.280Z",
     //   objectId : "teDOY3Rnpe",
@@ -50,54 +37,22 @@ exports.handleRequest = function(request, response) {
     //   updatedAt : "2013-10-07T16:22:03.280Z",
     //   username : "gary"
     // }]};
-
-    var testObject = {results : objContainer};
-
-    response.end(JSON.stringify(testObject));
-
-
-  } 
-
+    var responseObj = {results : objContainer};
+    sendResponse(response, 200, responseObj);
+  }
 
   if (request.method === 'POST') {
-
     var result = '';
     request.on('data', function(chunk){
-      // console.log(chunk);
       result += chunk;
     });
-
-    request.on('end', function(chunk){
-    // console.log('parsed result: ' + qs.parse(result).text);
-    objContainer.push(qs.parse(result));
-    console.log('objContainer length :', objContainer.length);
+    request.on('end', function(){
+      console.log(JSON.parse(result));
+      objContainer.unshift(JSON.parse(result));
+      console.log('objContainer length :', objContainer.length);
     });
+    sendResponse(response, 201, {});
+  }
 
-
-  } 
-
-  
-
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-   
-  response.end(JSON.stringify(testObject));
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
-    // console.log("*******************this is the request ****************"); 
-    // console.log(request);
-    // console.log("*******************this was the request ****************"); 
